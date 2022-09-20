e080e16a348c65519bdb824896183f565733b648
LFR <ooolfr@mail.ru>
2022-09-20 15:22:42 +0300

    add metod PrintArray

--- Program.cs
+++ Program.cs
@@ -4,5 +4,24 @@
     return Console.ReadLine()??"";
 }
 
+
+void PrintArray(string [] inputArray)
+{
+    Console.Write("[");
+    for(int i=0;i<inputArray.Length-1;i++)
+    {
+        if(inputArray[i]!="")
+        {
+            if(i==0) Console.Write(inputArray[i]);
+            else{Console.Write(","+ inputArray[i]);}
+        }
+    }
+    Console.Write("]");
+}
 string inputString=ReadData();
 Console.WriteLine(inputString);
+//пример строки ["hello", "2", "world", ":-)"]
+string inputStringFilter = inputString.Replace("[", "").Replace("]", "");
+Console.WriteLine(inputStringFilter);
+string [] inputArray=inputStringFilter.Split(", ");
+PrintArray(inputArray);
